1f4064b8b23779037b4405a86b0fb82bf70f1d72
shuhblam <745064+shuhblam@users.noreply.github.com>
2022-01-04 23:12:07 -0500

    final version

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-// these are the variables you can use as inputs to your algorithms
-console.log(fxhash)   // the 64 chars hex number fed to your algorithm
-console.log(fxrand()) // deterministic PRNG function, use it instead of Math.rand()
-var rnd;
+window.$fxhashFeatures = {
+  padding: padding,
+  scheme: "", 
+}
 
-var x1 = 0;
-var y1 = 0;
-var x2 = 0;
-var y2 = 0;
+var x1;
+var y1;
+var x2;
+var y2;
 var padding;
 var size;
 
-function rand(min, max) { 
+function rand(min, max) {
+  if(!min || !max) return fxrand()
   return fxrand() * (max - min) + min;
 }
 
@@ -18,36 +19,55 @@ function randPadding() {
 
 }
 
-window.$fxhashFeatures = {
-  rnd: rnd,
-  scheme: "", 
-}
-
-window.setup = function() {
-  size = min(windowWidth, windowHeight)
-  console.log(s)
-  createCanvas(size, size);
-  blendMode(MULTIPLY);
-  padding = int(rand(4,20))
-
-  while(x1 < size - padding*rand(1,2)+ size/8){
-    x1 = x1 + padding*rand()*2;
-    x2 = x2 + padding*rand()*2;
-    line(x1, y1, x2, size)
-  }
-  
+function init(){
   x1 = 0;
   y1 = 0;
   x2 = 0;
   y2 = 0;
+};
+
+
+window.setup = function() {
+
+  size = min(windowWidth, windowHeight)
+  console.log(fxhash)
+  background(255)
+  createCanvas(size, size);
+  blendMode(MULTIPLY);
+  padding = int(rand(3,20))
+
+  noLoop();
+}
+
+window.draw = function(){
+  background(255)
+  clear();
+  init();
+
+
+  while(x1 < size - padding*rand(1,2) + size/8){
+    var x1p = rand()*rand(1,2);
+    var x2p = rand()*rand(1,2);
+    x1 = x1 + padding*x1p;
+    x2 = x2 + padding*x2p;
+    line(x1, y1, x2, size)
+  }
+  
+  init();
 
   while(y1 < size - padding*rand(1,2) + size/8){
-    y1 = y1 + padding*rand();
-    y2 = y2 + padding*rand();
+    var y1p = rand()*rand(1,2);
+    var y2p = rand()*rand(1,2);
+    y1 = y1 + padding*y1p;
+    y2 = y2 + padding*y2p;
     line(x1, y1, size, y2)
   }
   
-  console.log(padding)
+  window.$fxhashFeatures = {
+    "loom room": padding,
+  }
+
+  console.log($fxhashFeatures)
 }
 window.mousePressed = function() {
   redraw();
