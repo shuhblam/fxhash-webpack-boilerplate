f5abbf027091bf7a30e6c9994226312b68ed20b9
shuhblam <745064+shuhblam@users.noreply.github.com>
2022-01-04 20:25:12 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,59 @@
 // these are the variables you can use as inputs to your algorithms
 console.log(fxhash)   // the 64 chars hex number fed to your algorithm
-console.log(fxrand()) // deterministic PRNG function, use it instead of Math.random()
+console.log(fxrand()) // deterministic PRNG function, use it instead of Math.rand()
+var rnd;
 
-// note about the fxrand() function 
-// when the "fxhash" is always the same, it will generate the same sequence of
-// pseudo random numbers, always
+var x1 = 0;
+var y1 = 0;
+var x2 = 0;
+var y2 = 0;
+var padding;
+var size;
 
-//----------------------
-// defining features
-//----------------------
-// You can define some token features by populating the $fxhashFeatures property
-// of the window object.
-// More about it in the guide, section features:
-// [https://fxhash.xyz/articles/guide-mint-generative-token#features]
-//
-// window.$fxhashFeatures = {
-//   "Background": "Black",
-//   "Number of lines": 10,
-//   "Inverted": true
-// }
+function rand(min, max) { 
+  return fxrand() * (max - min) + min;
+}
 
-// this code writes the values to the DOM as an example
-const container = document.createElement("div")
-container.innerText = `
-  random hash: ${fxhash}\n
-  some pseudo random values: [ ${fxrand()}, ${fxrand()}, ${fxrand()}, ${fxrand()}, ${fxrand()},... ]\n
-`
-document.body.prepend(container)
+function randPadding() {
+
+}
+
+window.$fxhashFeatures = {
+  rnd: rnd,
+  scheme: "", 
+}
+
+window.setup = function() {
+  size = min(windowWidth, windowHeight)
+  console.log(s)
+  createCanvas(size, size);
+  blendMode(MULTIPLY);
+  padding = int(rand(4,20))
+
+  while(x1 < size - padding*rand(1,2)+ size/8){
+    x1 = x1 + padding*rand()*2;
+    x2 = x2 + padding*rand()*2;
+    line(x1, y1, x2, size)
+  }
+  
+  x1 = 0;
+  y1 = 0;
+  x2 = 0;
+  y2 = 0;
+
+  while(y1 < size - padding*rand(1,2) + size/8){
+    y1 = y1 + padding*rand();
+    y2 = y2 + padding*rand();
+    line(x1, y1, size, y2)
+  }
+  
+  console.log(padding)
+}
+window.mousePressed = function() {
+  redraw();
+}
+
+window.keyPressed = function() {
+  //save();
+}
+        
