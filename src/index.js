689343750fe25a05bca51a44e77944961f4fe85f
shuhblam <745064+shuhblam@users.noreply.github.com>
2022-01-10 15:37:48 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,26 +5,21 @@ var colorScheme;
 
 
 import { getColorScheme } from './utils/colors';
+
 import { getRandomNumber as rndm, getRandomArrayItem } from './utils/random';
 
 window.setup = function() {
   colorMode(HSB, 360, 100, 100, 100);
   size = min(windowWidth, windowHeight)
   padding = size / 15;
-  innerSize = size - padding * 2;
+  innerSize = int(size - padding * 2);
 
-  colorScheme = getColorScheme(7);
+  colorScheme = getColorScheme();
 
   background(255)
   createCanvas(size, size);
 
   noLoop();
-
-}
-
-window.draw = function() {
-  background(255)
-  var color = getRandomArrayItem(colorScheme);
   console.log(JSON.stringify({
     fxhash,
     size,
@@ -35,9 +30,43 @@ window.draw = function() {
     random: rndm()
   }, null, 2));
 
+}
+
+window.draw = function() {
+  background(`255`)
+  var arr = [];
+  for(var i = 4; i < 250; i = i+4){
+    arr.push(i)
+  }
+  var sections = getRandomArrayItem(arr);
+  var xSections = innerSize / sections;
+  var ySections = innerSize / sections;
+  var x = 0 + padding;
+  var y = 0 + padding;
+  rect(padding, padding, innerSize, innerSize)
+
+
+
+  var scheme = JSON.parse(JSON.stringify(colorScheme));
+  scheme.push('#ffffff')
+  console.log(scheme)
+  for(var i = 0; i < sections; i++){
+    for(var j = 0; j < sections; j++){
+
+      var color = getRandomArrayItem(scheme);
+      //console.log(color)
+      fill(`${color}`);
+      stroke(`${color}`)
+      rect(x, y, xSections, ySections);
+      x += xSections;
+
+    }
+    x = 0 + padding;
+    y += ySections;
+  }
 
   noFill();
-  rect(padding, padding, innerSize, innerSize)
+
 }
 
 window.mousePressed = function() {
