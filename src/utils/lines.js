ec475474edcff15c4f82a2573e9abcc253d8e334
shuhblam <745064+shuhblam@users.noreply.github.com>
2022-01-10 15:37:03 -0500

    Create lines.js

---
+++ src/utils/lines.js
@@ -0,0 +1,14 @@
+import { getRandomNumber as rndm } from './random';
+
+export const line1 = (x1, y1, x2, y2, weight) => {
+  for (var i = 0; i < weight * 5; i++) {
+    var theta = rndm(0, TWO_PI);
+    var nx1 = x1 + rndm(0, weight / 2) * cos(theta);
+    var ny1 = y1 + rndm(0, weight / 2) * sin(theta);
+    var nx2 = x2 + rndm(0, weight / 2) * cos(theta);
+    var ny2 = y2 + rndm(0, weight / 2) * sin(theta);
+    strokeWeight(0.5);
+    line(nx1, ny1, nx2, ny2);
+    console.log('line1', TWO_PI, theta)
+  }
+}
